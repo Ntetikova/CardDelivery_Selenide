b4713b5bd9ffee3989518db18ca3a95912f6fa50
Ntetikova <Ntetikova@gmail.com>
2023-05-10 16:54:36 +0300

    Changed tests

--- src/test/java/ru/netology/CardDeliveryTest.java
+++ src/test/java/ru/netology/CardDeliveryTest.java
@@ -26,10 +26,11 @@ public class CardDeliveryTest {
     }
 
     @Test
-    public void SendFormHappyPath() {
+    public void sendFormHappyPath() {
         String meetingDate = generateDate(3, "dd.MM.yyyy");
 
         $("[data-test-id='city'] input").setValue("Москва");
+        $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
         $("[data-test-id='date'] input").setValue(meetingDate);
         $("[data-test-id='name'] input").setValue("Супер Олег");
         $("[name='phone']").setValue("+79520010203");
@@ -41,8 +42,11 @@ public class CardDeliveryTest {
 
     @Test
     public void sendFormWrongCity() {
+        String meetingDate = generateDate(3, "dd.MM.yyyy");
 
         $("[data-test-id='city'] input").setValue("Гагарин");
+        $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
+        $("[data-test-id='date'] input").setValue(meetingDate);
         $("[data-test-id='name'] input").setValue("Супер Олег");
         $("[name='phone']").setValue("+79520010203");
         $("[data-test-id='agreement']").click();
@@ -52,7 +56,10 @@ public class CardDeliveryTest {
 
     @Test
     public void sendFormEmptyCity() {
+        String meetingDate = generateDate(3, "dd.MM.yyyy");
 
+        $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
+        $("[data-test-id='date'] input").setValue(meetingDate);
         $("[data-test-id='name'] input").setValue("Супер Олег");
         $("[name='phone']").setValue("+79520010203");
         $("[data-test-id='agreement']").click();
@@ -62,8 +69,11 @@ public class CardDeliveryTest {
 
     @Test
     public void sendFormEmptyName() {
+        String meetingDate = generateDate(3, "dd.MM.yyyy");
 
         $("[data-test-id='city'] input").setValue("Смоленск");
+        $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
+        $("[data-test-id='date'] input").setValue(meetingDate);
         $("[name='phone']").setValue("+79520010203");
         $("[data-test-id='agreement']").click();
         $$("button").find(exactText("Забронировать")).click();
@@ -72,8 +82,11 @@ public class CardDeliveryTest {
 
     @Test
     public void sendFormEmptyPhone() {
+        String meetingDate = generateDate(3, "dd.MM.yyyy");
 
         $("[data-test-id='city'] input").setValue("Смоленск");
+        $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
+        $("[data-test-id='date'] input").setValue(meetingDate);
         $("[data-test-id='name'] input").setValue("Супер Олег");
         $("[data-test-id='agreement']").click();
         $$("button").find(exactText("Забронировать")).click();
@@ -82,8 +95,11 @@ public class CardDeliveryTest {
 
     @Test
     public void sendFormWrongName() {
+        String meetingDate = generateDate(3, "dd.MM.yyyy");
 
         $("[data-test-id='city'] input").setValue("Москва");
+        $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
+        $("[data-test-id='date'] input").setValue(meetingDate);
         $("[data-test-id='name'] input").setValue("Super Oleg");
         $("[name='phone']").setValue("+79520010203");
         $("[data-test-id='agreement']").click();
@@ -93,8 +109,11 @@ public class CardDeliveryTest {
 
     @Test
     public void sendFormWrongPhone() {
+        String meetingDate = generateDate(3, "dd.MM.yyyy");
 
         $("[data-test-id='city'] input").setValue("Москва");
+        $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
+        $("[data-test-id='date'] input").setValue(meetingDate);
         $("[data-test-id='name'] input").setValue("Супер Олег");
         $("[name='phone']").setValue("89520010203");
         $("[data-test-id='agreement']").click();
@@ -103,15 +122,25 @@ public class CardDeliveryTest {
     }
 
     @Test
-    public void sendFormAutoSelectCity() {
-        String meetingDate = generateDate(3, "dd.MM.yyyy");
+    public void sendFormAutoSelectCityAndMeetingDate() {
+
 
         $("[data-test-id='city'] input").sendKeys("Мо");
         ElementsCollection city = $$("div.menu-item");
         city.get(2).click();
 
-        // $("[data-test-id='date'] input").doubleClick().sendKeys(Keys.BACK_SPACE);
-        // $("[data-test-id='date'] input").sendKeys(Keys.CONTROL, Keys.DELETE);
+        $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
+
+        String thisMonth = generateDate(0, "M");
+        String meetingMonth = generateDate(32, "M");
+
+        if (!thisMonth.equals(meetingMonth)) {
+            $("div.calendar__arrow_direction_right[data-step='1']").click();
+        }
+
+        ElementsCollection days = $$("td.calendar__day");
+       String meetingDate = generateDate(0, "d");
+        days.findBy(exactText(meetingDate)).click();
         $("[data-test-id='date'] input").setValue(meetingDate);
         $("[data-test-id='name'] input").setValue("Череззаборногузадерищенский Петр");
         $("[data-test-id='phone'] input").setValue("+79012345678");
